ac5e2645a0ac4d08db6f22bdafd5f8901cf53ff0
DMZTdhruv <shruvparmar.gmail.com>
2023-06-13 20:11:11 +0530

    Improved password generator

--- password.py
+++ password.py
@@ -1,33 +1,54 @@
 import random
 
 print("Welcome to password Generator!")
-# Password will be generated from this:
-uppercase_letters = "ABCDEFGHIJKLMNAOPQRSTUVXYZ"
-lowercase_letters = uppercase_letters.lower()
-numbers = "1234567890"
-symbols = "~!@#$%^&*()"
+name = input("Enter your name please: ")
 
-# The password will be generated based on the given options:
-include_uppercase_letters = "false"
-include_lowercase_letters = "false"
-include_numbers = "false"
-include_symbols = "false"
+upper_case_letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+lower_case_letters = upper_case_letters.lower();
+symbols = "`~!@#$%^&*()"
+numbers = "0123456789"
 
-# User input:
-print("Enter 'true' or 'false' as answers! ")
-include_uppercase_letters = input("Do you want 'UPPERCASE' letters in your password?: ")
-include_lowercase_letters = input("Do you want 'lowercase' letters in your password?: ")
-include_symbols_letters = input("Do you want 'symbols' letters in your password?: ")
-include_numbers_letters = input("Do you want 'numbers' letters in your password?: ")
+print("Enter the following questions to generate your password: ")
+include_uppercase_letters = "N"
+include_lowercase_letters = "N"
+include_symbols = "N"
+include_numbers = "N"
 
-# Password generating logic:
-length_of_password = int(input("Enter your password length: "))
-password = ""
-for i in range(0,length_of_password):
-    if include_uppercase_letters == "true" : password += uppercase_letters[int(random.random() * len(uppercase_letters))]
-    if include_lowercase_letters == "true" : password += lowercase_letters[int(random.random() * len(lowercase_letters))]
-    if include_numbers_letters == "true"   : password += numbers[int(random.random() * len(numbers))]
-    if include_symbols_letters == "true"   : password += symbols[int(random.random() * len(symbols))]
 
-# Lenght of password asking from user
-print(password[0:length_of_password])
+password = [];
+include_uppercase_letters = input("Do you want Uppercase characters in your password Y or N: ")
+if include_uppercase_letters == "Y": 
+    length = int(input("How many uppercase Letters do you want: "))
+    for i in range(length):
+        password += upper_case_letters[int(random.random() * len(upper_case_letters))]
+
+include_lowercase_letters = input("Do you want Lowercase characters in your password Y or N: ")
+if include_lowercase_letters == "Y":
+    length = int(input("How many Lowercase Letters  do you want: "))
+    for i in range(length):
+        password += lower_case_letters[int(random.random() * len(lower_case_letters))]
+
+include_numbers = input("Do you want Numbers in your password Y or N: ")
+if include_numbers == "Y":
+    length = int(input("How many Numbers do you want: "))
+    for i in range(length):
+        password += numbers[int(random.random() * len(numbers))]
+
+include_symbols = input("Do you want symbols in your password Y or N: ")
+if include_symbols == "Y":
+    length = int(input("How many Symbols do you want: "))
+    for i in range(length):
+        password += symbols[int(random.random() * len(symbols))]
+
+
+print(f"{name} based on what you selected we have made this password for you: ")
+random.shuffle(password);
+real_password = ''.join(password);
+print(real_password);
+
+if len(password) > 10:
+    print("That's a strong password!")
+elif len(password) > 8:
+    print("That's a good password!");
+else:
+    print("That's a very easy password")
